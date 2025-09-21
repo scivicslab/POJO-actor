92dfd7a2812b0e7c338888bf18d039f3f33d6834
scivicslab <devteam@scivics-lab.com>
2025-09-21 15:53:35 +0900

    Update javadoc site

--- deploy_site.sh
+++ deploy_site.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+set -euo pipefail
+
 export LANG=en_US.UTF-8
 mvn javadoc:javadoc
 
 dest=$PWD/docs
 
 # Create destination directory if it doesn't exist
-mkdir -p ${dest}
+mkdir -p "${dest}"
 
-# Remove existing docs if they exist
-rm -Rf ${dest}/*
+# Remove existing docs if they exist (preserving dotfiles like .nojekyll)
+rm -Rf "${dest}"/*
 
 # Move generated javadoc to destination
-mv target/site/apidocs/* ${dest}/
+mv target/site/apidocs/* "${dest}/"
+
+# Ensure GitHub Pages does not run Jekyll on the generated site
+touch "${dest}/.nojekyll"
 
